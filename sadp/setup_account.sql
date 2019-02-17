be6b5a84a93899c4d7d2d182802e58d4fa2a2c1c
Loi Cao <Loi.Cao@lexisnexisrisk.com>
2019-02-17 16:05:46 +0700

    add ingress and account configuration

---
+++ sadp/setup_account.sql
@@ -0,0 +1,2 @@
+insert into events values ('00000168fa933569-0242ac1100110000', 'net.chrisrichardson.ftgo.accountingservice.domain.AccountAuthorizedEvent', '{}', 'net.chrisrichardson.ftgo.accountingservice.domain.Account', 1, '{}', '{}', null);
+insert into entities values ('net.chrisrichardson.ftgo.accountingservice.domain.Account', 1, '00000168fa933569-0242ac1100110000');
